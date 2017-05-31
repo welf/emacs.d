6fd322073514ebd96738a0f441bf7b2acae0d252
Arthur Welf <AW@MBPro-AW.station>
2017-05-31 03:53:35 +0200

    Add some new packages and configure src-blocks in org-mode

--- custom.el
+++ custom.el
@@ -6,7 +6,8 @@
  '(org-export-backends (quote (ascii beamer html latex md)))
  '(package-selected-packages
    (quote
-    (magit fancy-battery info+ google-this which-key undo-tree ag markdown-mode flycheck-elm elm-mode ox-twbs org-bullets org-plus-contrib hamlet-mode hayoo hindent intero flycheck-hdevtools flycheck-haskell json-reformat mmm-mode smex helm-descbinds helm-swoop helm-ag helm-projectile helm flycheck smart-tab company-statistics company-quickhelp company eshell-prompt-extras with-editor visual-regexp-steroids visual-regexp whole-line-or-region browse-kill-ring expand-region move-text multiple-cursors mwim aggressive-indent indent-guide rainbow-delimiters paredit-everywhere paredit page-break-lines hlinum ace-window popwin rich-minority spaceline-all-the-icons rainbow-mode flatui-theme solarized-theme f dash-at-point paradox async auto-package-update exec-path-from-shell use-package benchmark-init)))
+    (magit-gh-pulls floobits scratch restclient olivetti fountain-mode ein python-mode skewer-mode emmet-mode web-mode yasnippet subatomic-theme waher-theme espresso-theme doom-themes magit fancy-battery info+ google-this which-key undo-tree ag markdown-mode flycheck-elm elm-mode ox-twbs org-bullets org-plus-contrib hamlet-mode hayoo hindent intero flycheck-hdevtools flycheck-haskell json-reformat mmm-mode smex helm-descbinds helm-swoop helm-ag helm-projectile helm flycheck smart-tab company-statistics company-quickhelp company eshell-prompt-extras with-editor visual-regexp-steroids visual-regexp whole-line-or-region browse-kill-ring expand-region move-text multiple-cursors mwim aggressive-indent indent-guide rainbow-delimiters paredit-everywhere paredit page-break-lines hlinum ace-window popwin rich-minority spaceline-all-the-icons rainbow-mode flatui-theme solarized-theme f dash-at-point paradox async auto-package-update exec-path-from-shell use-package benchmark-init)))
+ '(paradox-automatically-star t)
  '(solarized-contrast (quote high))
  '(solarized-termcolors 256)
  '(solarized-visibility (quote high)))
@@ -15,4 +16,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-block ((t (:background "#FFFFEA" :foreground "#93a1a1"))))
+ '(org-block-begin-line ((t (:background "#fdf6e3" :foreground "#93a1a1" :slant italic :weight bold)))))
