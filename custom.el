a84845bdabf9b450f1601a5010709bf765ddc97f
Arthur Welf <AW@MBPro-AW.station>
2017-09-11 21:37:06 +0200

    Added and edited some package settings

--- custom.el
+++ custom.el
@@ -3,11 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(hl-sexp-background-color "#1c1f26")
  '(org-export-backends (quote (ascii beamer html latex md)))
+ '(org-fontify-quote-and-verse-blocks t)
  '(package-selected-packages
    (quote
-    (magit-gh-pulls floobits scratch restclient olivetti fountain-mode ein python-mode skewer-mode emmet-mode web-mode yasnippet subatomic-theme waher-theme espresso-theme doom-themes magit fancy-battery info+ google-this which-key undo-tree ag markdown-mode flycheck-elm elm-mode ox-twbs org-bullets org-plus-contrib hamlet-mode hayoo hindent intero flycheck-hdevtools flycheck-haskell json-reformat mmm-mode smex helm-descbinds helm-swoop helm-ag helm-projectile helm flycheck smart-tab company-statistics company-quickhelp company eshell-prompt-extras with-editor visual-regexp-steroids visual-regexp whole-line-or-region browse-kill-ring expand-region move-text multiple-cursors mwim aggressive-indent indent-guide rainbow-delimiters paredit-everywhere paredit page-break-lines hlinum ace-window popwin rich-minority spaceline-all-the-icons rainbow-mode flatui-theme solarized-theme f dash-at-point paradox async auto-package-update exec-path-from-shell use-package benchmark-init)))
- '(paradox-automatically-star t)
+    (html-mode blackboard-theme atom-one-dark-theme auto-complete base16-theme material-theme magit-gh-pulls floobits scratch restclient olivetti fountain-mode ein python-mode skewer-mode emmet-mode web-mode yasnippet subatomic-theme waher-theme espresso-theme doom-themes magit fancy-battery info+ google-this which-key undo-tree ag markdown-mode flycheck-elm elm-mode ox-twbs org-bullets org-plus-contrib hamlet-mode hayoo hindent intero flycheck-hdevtools flycheck-haskell json-reformat mmm-mode smex helm-descbinds helm-swoop helm-ag helm-projectile helm flycheck smart-tab company-statistics company-quickhelp company eshell-prompt-extras with-editor visual-regexp-steroids visual-regexp whole-line-or-region browse-kill-ring expand-region move-text multiple-cursors mwim aggressive-indent indent-guide rainbow-delimiters paredit-everywhere paredit page-break-lines hlinum ace-window popwin rich-minority spaceline-all-the-icons rainbow-mode flatui-theme solarized-theme f dash-at-point paradox async auto-package-update exec-path-from-shell use-package benchmark-init)))
+ '(paradox-github-token t)
  '(solarized-contrast (quote high))
  '(solarized-termcolors 256)
  '(solarized-visibility (quote high)))
@@ -16,5 +20,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-block ((t (:background "#FFFFEA" :foreground "#93a1a1"))))
- '(org-block-begin-line ((t (:background "#fdf6e3" :foreground "#93a1a1" :slant italic :weight bold)))))
+ '(magit-popup-disabled-argument ((t (:bold nil :background nil :underline nil :italic t))))
+ '(markdown-markup-face ((t (:bold nil :background nil)))))
+
+
+;;; Sometimes Emacs overrides these settings, so we'll store them in the comment:
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(markdown-markup-face ((t (:bold nil :background nil))))
+;;  '(magit-popup-disabled-argument ((t (:bold nil :background nil :underline nil :italic t)))))
